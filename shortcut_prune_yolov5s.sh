d6fa8509c444179d220ec6bc089d37b985869d10
ls <qqlishuang@gmail.com>
2021-11-29 10:12:23 +0800

    update other prune methods

--- shortcut_prune_yolov5s.sh
+++ shortcut_prune_yolov5s.sh
@@ -1,1 +1,1 @@
-python shortcut_prune_yolov5s.py --cfg cfg/yolov5s_v4_hand.cfg --data data/oxfordhand.data --weights weights/last_v4s.pt --percent 0.5 --img_size 416
+python shortcut_prune_yolov5s.py --cfg cfg/yolov5s_v6_hand.cfg --data data/oxfordhand.data --weights weights/last_v6s.pt --percent 0.5 --img_size 640
